a841eb90630a54847ac1172b36539e68edf2a375
Nap Ramirez <napramirez@gmail.com>
2022-05-09 20:48:49 +0800

    Added provisioner for `azure-cli`

---
+++ scripts/install-azure-cli.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+#
+
+set -e
+
+apt-get update && \
+    apt-get install -y \
+        ca-certificates \
+        curl \
+        apt-transport-https \
+        lsb-release \
+        gnupg
+
+export MICROSOFT_ARCHIVE_KEYRING=/etc/apt/trusted.gpg.d/microsoft.gpg
+rm -f $MICROSOFT_ARCHIVE_KEYRING && \
+    curl -sL https://packages.microsoft.com/keys/microsoft.asc \
+        | gpg --dearmor -o $MICROSOFT_ARCHIVE_KEYRING
+
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=$MICROSOFT_ARCHIVE_KEYRING] \
+    https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" \
+        | tee /etc/apt/sources.list.d/azure-cli.list > /dev/null
+
+apt-get update && \
+    apt-get install -y \
+        azure-cli
